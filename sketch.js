fb8fed3ea1212be56e767bde9123224453875622
tcc545-art <tcc545@york.ac.uk>
2025-10-11 19:43:45 +0100

    Update sketch.js

---
+++ sketch.js
@@ -0,0 +1,15 @@
+const GRID_SIZE = 20;
+let headX = 60
+let headY = 60
+let foodX = 20
+let foodY = 20
+
+function setup(){
+    createCanvas(600,600);
+
+}
+function draw(){
+    background(0)
+
+
+}
